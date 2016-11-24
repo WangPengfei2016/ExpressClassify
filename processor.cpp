07b00bcaf6ad4f9c871b1ea8e9bd370fac929382
黄金 <huangjin_henan@outlook.com>
2016-11-24 17:37:08 +0800

    过滤条形码

--- processor.cpp
+++ processor.cpp
@@ -305,9 +305,10 @@ string Processor::extract_phone(std::string path, int width, int height)
 
         // 开始识别手机号
         string num = recognize_num(candidate_region);
-		size_t pos = path.find_first_of(num);
+		size_t pos = string::npos;
+		pos = path.find(num);
 
-		if (pos == string::npos) {
+		if (pos != string::npos) {
 			num = "NO";
 		}
         if (num != "NO")
